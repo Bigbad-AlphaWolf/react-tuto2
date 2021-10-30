12dc7825172db0a81849d0b6a8b53a58986f04ec
Mohamed Abdoul Karim Diop <mohamedabdoulkarim.diop2@orange-sonatel.com>
2021-10-30 22:29:23 +0000

    First react app with data handling through formulaire

---
+++ src/Form.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+
+class Form extends Component {
+    initialState = {
+        name: '',
+        job: ''
+    };
+
+    state = this.initialState;
+
+    handleChange = event => {
+        const {name, value} = event.target;
+
+        this.setState({
+            [name]: value
+        });
+    };
+
+    submitForm = () => {
+        this.props.handleSubmit(this.state);
+        this.setState(this.initialState);
+    };
+
+    render() {
+        const {name, job} = this.state;
+
+        return (
+            <form>
+                <label htmlFor='name'>Name</label>
+                <input type='text' name='name' id='name' value={name} onChange={this.handleChange} />
+                <br />
+                <label htmlFor='job'>Job</label>
+                <input type='text' name='job' id='job' value={job} onChange={this.handleChange} />
+                <p>
+                    <input type='button' value='Submit' onClick={this.submitForm} />
+                </p>
+            </form>
+        );
+    }
+}
+
+export default Form;
